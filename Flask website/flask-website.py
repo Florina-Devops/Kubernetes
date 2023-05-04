312cca0d2ea28be8b23e97cb902f3907ef305ec5
Florina-Devops <florinanelson597@gmail.com>
2023-05-04 18:20:22 +0200

    changed folder structure

--- Flask website/flask-website.py
+++ Flask website/flask-website.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello_world():
     return "<p>This is a Hello World application</p>"
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=1234,debug=True)
+    app.run(host='0.0.0.0', port='1234',debug=True)
